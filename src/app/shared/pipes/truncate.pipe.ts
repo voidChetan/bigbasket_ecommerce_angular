9e4ef3144a07beccf7d59184e229843157eece33
codefirst2022 <codefirst2022@gmail.com>
2024-04-24 12:08:00 +0530

    chnages

--- src/app/shared/pipes/truncate.pipe.ts
+++ src/app/shared/pipes/truncate.pipe.ts
@@ -7,10 +7,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TruncatePipe implements PipeTransform {
 
   transform(text: string, maxLength: number = 20): string {
-    if (text.length <= maxLength) {
-      return text;
+    if(text) {
+      if (text && text.length <= maxLength) {
+        return text;
+      }
+      return text.substring(0, maxLength) + '...';
+    } else {
+      return '--';
     }
-    return text.substring(0, maxLength) + '...';
+   
   }
 
 }
